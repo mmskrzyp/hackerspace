20fc2175bc9a8160893107282b740b6a7b7a8a44
Maria Skrzypek <mmskrzyp@gmail.com>
2012-05-29 21:23:20 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerspace::Application.config.secret_token = 'b32dede79da5f00b8287266f6106fd7e4d5dd70167849d8ce5396a4c79b0d77cc5b859eaa1ed75b5e9e7366e5b404921f7c0c8c47e11c723a32444f2fd9f997d'
