d9bf57eb7c6b8b2845f47861adcd16f3a9099c29
Maria Skrzypek <mmskrzyp@gmail.com>
2012-05-31 02:17:59 +0200

    Added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def sponsors
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Hackerspace::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/contact"
+
+  get "static_pages/about"
+
+  get "static_pages/sponsors"
+
   resources :projects
 
   resources :hackers
