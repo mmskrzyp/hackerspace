21074183da4b6bcc66dc9f1501667414786e99c7
Przemek Hocke <phocke@gmail.com>
2012-09-20 02:08:22 +0200

    get the order right

--- app/constraints/static_page_constraint.rb
+++ app/constraints/static_page_constraint.rb
@@ -1,5 +1,5 @@
 class StaticPageConstraint
-  STATIC_PAGES = %w(about contact events home sponsors partners newbie1 newbie2 newbie3)
+  STATIC_PAGES = %w(about projects events hackers sponsors partners contact home newbie1 newbie2 newbie3)
 
 
   def self.matches?(request)
